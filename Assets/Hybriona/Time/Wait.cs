482867118c13fbcdce87e4720807c6adfc6b7c6e
Rajkumar Pramanik (RJproz) <rjproz@gmail.com>
2021-05-25 14:31:19 +0530

    - Wait Script

---
+++ Assets/Hybriona/Time/Wait.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using UnityEngine;
+namespace Hybriona
+{
+    public class Wait
+    {
+        //new WaitForSeconds
+        public static IEnumerator WaitForSeconds(float wait)
+        {
+            float timer = 0;
+            while (timer <= wait)
+            {
+                timer += Time.deltaTime;
+                yield return null;
+            }
+        }
+
+
+        // new WaitForSecondsRealtime
+        public static IEnumerator WaitForSecondsRealtime(float wait)
+        {
+            float timer = 0;
+            while (timer <= wait)
+            {
+                timer += Time.unscaledDeltaTime;
+                yield return null;
+            }
+        }
+
+        // new WaitForSecondsRealtime
+        public static IEnumerator WaitForFixedSeconds(float wait)
+        {
+            float timer = 0;
+            while (timer <= wait)
+            {
+                timer += Time.fixedDeltaTime;
+                yield return null;
+            }
+        }
+    }
+}
